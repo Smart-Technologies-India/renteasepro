d8a6c4561f32f659f870efaa3d7df3afaac93385
analog-sombra <analogsombra@gmail.com>
2024-07-28 18:00:12 +0530

    build: github action add

---
+++ .scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started..."
+
+# Pull the latest version of the app
+git pull origin master
+echo "New changes copied to server !"
+
+echo "Installing Dependencies..."
+pnpm install
+
+echo "Updating database..."
+pnpm exec prisma db push
+
+echo "Creating Production Build..."
+pnpm build
+
+echo "PM2 Reload"
+pm2 reload rentpro
+
+echo "Deployment Finished!"
