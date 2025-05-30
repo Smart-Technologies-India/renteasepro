151df5b26f59e3433d1efb97ebd4d7ddfb5b79c0
analog-sombra <analogsombra@gmail.com>
2025-05-30 14:24:07 +0530

    build: swiper photo update

--- src/app/dashboard/dailyshops/details/[id]/shopview.tsx
+++ src/app/dashboard/dailyshops/details/[id]/shopview.tsx
@@ -295,7 +295,6 @@ const ShopView = (props: ShowShopProps) => {
             {shop?.daily_rent_photo.map((photo, index) => (
               <SwiperSlide key={index}>
                 <div className="relative aspect-video">
-                  <p>{photo.path}</p>
                   <Image
                     src={photo.path}
                     alt="Property"
